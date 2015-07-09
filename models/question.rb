ee1b395f265f25ab6e3f7d229d7ff12611a16921
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-09 14:45:38 -0500

    Working model

--- models/question.rb
+++ models/question.rb
@@ -3,17 +3,20 @@ require_relative "database_instance_methods"
 
 class Question
   
+  include DatabaseInstanceMethods
+  extend DatabaseClassMethods
+  
   attr_reader :id, :question, :choices, :answer
   
   def initialize(args)
-    @question = args["question"]
-    @choices = args["choices"] || []
-    @answer = args["answer"]
     @id = args["id"]
+    @question = args["question"]
+    @choices = args["choices"] || get_choices
+    @answer = args["answer"]
   end
   
   def get_choices
-    
+    @choices = CONNECTION.execute("SELECT * FROM choices WHERE question_id = #{@id};")
   end
   
 end
